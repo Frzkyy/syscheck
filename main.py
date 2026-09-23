2f01934389a519a0b0c0897ce5ab38c397557616
Frzkyy <fairuzzacky@gmail.com>
2026-09-23 12:53:18 +0700

    main menu

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from validator import Validator
-numer = Validator.Number.integer_input_range(50,error_message="Bodoh Anjing",print_message=": ")
-print(numer)
+numInput = Validator.Number
+def show_menu():
+     return"""============================
+1.Show device properties
+============================
+"""
+
+is_running = True
+while is_running:
+     print(show_menu(), end="")
+     choise = numInput.integer_input_rangeot(1,error_message="Only integer allowed",print_message="> ", error_range_message="Only (0-1) allowed")
+     match choise:
+          case 0:
+               is_running = False
+          case 1:
+               print("Hola",end="\n\n")
--- validator.py
+++ validator.py
@@ -1,56 +1,78 @@
 class Validator:
      class Number:
-
+          # ==================== INTEGER ====================
           #only integer
           @staticmethod
-          def integer_input(print_message=None,error_message=None):
-               is_running = True
-               while is_running:
+          def integer_input(print_message=None, error_message=None):
+               while True:
                     try:
-                         if print_message:
-                              number = int(input(print_message))
-                         else:
-                              number = int(input(""))
-                         return number
+                         return int(input(print_message or ""))
+
                     except ValueError:
                          if error_message:
-                              print(error_message, end="")
-                              input()
+                              print(error_message)
 
-          #integer with range (minimum = 0)
+
+          #integer with top limit (bottom =0)
           @staticmethod
-          def integer_input_range(end,print_message=None,error_message=None,error_range_message=None):
-               is_running = True
-               while is_running:
-                    try:
-                         if print_message:
-                              number = int(input(print_message))
-                         else:
-                              number = int(input())
+          def integer_input_rangeot(top,print_message=None,error_message=None,error_range_message=None):
+               while True:
+                    number = Validator.Number.integer_input(print_message,error_message)
 
-                         if number > end or number < 0:
-                              if error_range_message:
-                                   print(error_range_message)
-                              continue
-                         
+                    if 0 <= number <= top:
                          return number
-                    except ValueError:
-                         if error_message:
-                              print(error_message, end="")
-                              input()
 
+                    if error_range_message:
+                         print(error_range_message)
+
+
+          #integer with custom range
           @staticmethod
-          def float_input(print_message=None,error_message=None):
-               is_running = True
-               while is_running:
-                    try:
-                         if print_message:
-                              number = float(input(print_message))
-                         else:
-                              number = float(input())
+          def integer_input_range(bottom,top, print_message=None,error_message=None,error_range_message=None):
+               while True:
+                    number = Validator.Number.integer_input(print_message,error_message)
+
+                    if bottom <= number <= top:
                          return number
+
+                    if error_range_message:
+                         print(error_range_message)
+
+
+          # ==================== FLOAT ====================
+          #only float
+          @staticmethod
+          def float_input(print_message=None, error_message=None):
+               while True:
+                    try:
+                         return float(input(print_message or ""))
+
                     except ValueError:
                          if error_message:
-                              print(error_message, end="")
-                              input()
-                    
+                              print(error_message)
+
+
+          #float with top limit(bottom = 0)
+          @staticmethod
+          def float_input_rangeot(top,print_message=None,error_message=None,error_range_message=None):
+               while True:
+                    number = Validator.Number.float_input(print_message,error_message)
+
+                    if 0 <= number <= top:
+                         return number
+
+                    if error_range_message:
+                         print(error_range_message)
+
+
+          #float with custom range
+          @staticmethod
+          def float_input_range(bottom,top,print_message=None,error_message=None,error_range_message=None):
+               while True:
+                    number = Validator.Number.float_input(print_message,error_message)
+
+                    if bottom <= number <= top:
+                         return number
+
+                    if error_range_message:
+                         print(error_range_message)
